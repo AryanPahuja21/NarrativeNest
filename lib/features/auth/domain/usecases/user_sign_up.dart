d79b5bfa3f5489f1432df1ad10a4f3e71e07d66e
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-04-04 20:36:49 +0530

    auth use cases added

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,26 @@
+import 'package:fpdart/src/either.dart';
+import 'package:startup_draft1/core/error/failures.dart';
+import 'package:startup_draft1/core/usecase/usecase.dart';
+import 'package:startup_draft1/features/auth/domain/repository/auth_repository.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  const UserSignUp(this.authRepository);
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+        name: params.name, email: params.email, password: params.password);
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+}
